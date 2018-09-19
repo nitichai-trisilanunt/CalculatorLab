3760ef6d8ac616a58b68ee1e2f364ecca0a22be1
NItichai Trisilanunt <niti_chai_@hotmail.com>
2018-09-19 15:24:52 +0700

    011-600611019 lab2

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -1,17 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
         public string Process(string str)
         {
-            // your code here
-            return "E";
+            Stack rpnStack = new Stack();
+            string firstOp, secondOp;
+            string[] strArray = str.Split(' ');
+            if (strArray.Length < 3)
+            {
+                return "E";
+            }
+            else if (isOperator(strArray[1]) || isOperator(strArray[0]))
+            {
+                return "E";
+            }
+            else
+            {
+                foreach (string s in strArray)
+                {
+
+
+                    if (isNumber(s))
+                    {
+                        rpnStack.Push(s);
+                    }
+                    else if (rpnStack.Count > 1)
+                    {
+                        if (isOperator(s))
+                        {
+                            secondOp = rpnStack.Pop().ToString();
+                            firstOp = rpnStack.Pop().ToString();
+                            if (firstOp == null || secondOp == null)
+                            {
+                                return "E";
+                            }
+                            else
+                            {
+                                rpnStack.Push(calculate(s, firstOp, secondOp));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return "E";
+                    }
+                }
+                if (rpnStack.Count == 1)
+                {
+                    return decimal.Parse(rpnStack.Peek().ToString()).ToString("G29");
+                }
+                else
+                {
+                    return "E";
+                }
+            }
         }
     }
 }
