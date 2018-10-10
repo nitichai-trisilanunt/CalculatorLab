6f804082066446a239970cf0592a75d9d0b6df07
NItichai Trisilanunt <niti_chai_@hotmail.com>
2018-10-10 13:58:07 +0700

    600611019 lab4

--- CPE200Lab1/CPE200Lab1Tests/RPNCalculatorEngineTests.cs
+++ CPE200Lab1/CPE200Lab1Tests/RPNCalculatorEngineTests.cs
@@ -5,120 +5,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace CPE200Lab1.Tests
+namespace CPE200Lab1.test
 {
     [TestClass()]
-    public class RPNCalculatorEngineTests
+    class RPNCalculatorEngineTests
     {
-        RPNCalculatorEngine engine;
-
-        [TestInitialize()]
-        public void Initialize()
-        {
-            engine = new RPNCalculatorEngine();
-        }
-
         [TestMethod()]
-        public void ConstructorTest()
+        public void Process_Basic_Test()
         {
-            Assert.IsNotNull(engine);
-            Assert.IsInstanceOfType(engine, typeof(RPNCalculatorEngine));
+            RPNCalculatorEngine r = new RPNCalculatorEngine();
+            Assert.AreEqual("2", r.Process("1 1 +"));
+            Assert.AreEqual("1", r.Process("3 2 -"));
+            Assert.AreEqual("6", r.Process("3 2 X"));
+            Assert.AreEqual("2", r.Process("4 2 ÷"));
+            Assert.AreEqual("0", r.Process("1 -1 +"));
+            Assert.AreEqual("5", r.Process("3 -2 -"));
+            Assert.AreEqual("-1", r.Process("2 3 -"));
+            Assert.AreEqual("-6", r.Process("3 -2 X"));
+            Assert.AreEqual("-2", r.Process("-4 2 ÷"));
         }
-
         [TestMethod()]
-        public void EmptyArgumentTest()
+        public void Process_Complex_Test()
         {
-            Assert.AreEqual("E", engine.Process(null));
-            Assert.AreEqual("E", engine.Process(""));
+            RPNCalculatorEngine r = new RPNCalculatorEngine();
+            Assert.AreEqual("8", r.Process("1 3 + 2 X"));
+            Assert.AreEqual("5", r.Process("1 3 2 + X"));
+            Assert.AreEqual("-5", r.Process("1 2 3 4 + - X"));
         }
-
         [TestMethod()]
-        public void BasicCalcuationOneTest()
+        public void Process_Error_Test()
         {
-            Assert.AreEqual("0", engine.Process("0"));
+            RPNCalculatorEngine r = new RPNCalculatorEngine();
+            Assert.AreEqual("E", r.Process("1"));
+            Assert.AreEqual("E", r.Process("1 +"));
+            Assert.AreEqual("E", r.Process("1 + 1"));
+            Assert.AreEqual("E", r.Process("1 1 1 +"));
+            Assert.AreEqual("E", r.Process("+"));
+            Assert.AreEqual("E", r.Process("+ 1"));
+            Assert.AreEqual("E", r.Process("+ 1 1"));
+            Assert.AreEqual("E", r.Process("1 1 1 + + +"));
 
-            Assert.AreEqual("2", engine.Process("1 1 + "));
-            Assert.AreEqual("0", engine.Process("1 1 - "));
-            Assert.AreEqual("1", engine.Process("1 1 X "));
-            Assert.AreEqual("1", engine.Process("1 1 ÷ "));
-        }
-
-        [TestMethod()]
-        public void BasicCalcuationTwoTest()
-        {
-            Assert.AreEqual("10", engine.Process("5 5 + "));
-            Assert.AreEqual("10", engine.Process("20 10 - "));
-            Assert.AreEqual("10", engine.Process("5 2 X "));
-            Assert.AreEqual("10", engine.Process("10 1 ÷"));
-        }
-
-        [TestMethod()]
-        public void BasicCalcuationThreeTest()
-        {
-            Assert.AreEqual("20", engine.Process("10 10 + "));
-            Assert.AreEqual("-10", engine.Process("10 20 - "));
-            Assert.AreEqual("100", engine.Process("10 10 X "));
-            Assert.AreEqual("10", engine.Process("100 10 ÷"));
-        }
-
-        [TestMethod()]
-        public void BasicCalcuationFourTest()
-        {
-            Assert.AreEqual("-1", engine.Process("1 2 - "));
-            Assert.AreEqual("-1", engine.Process("-1 1 X "));
-            Assert.AreEqual("-1", engine.Process("1 -1 X "));
-            Assert.AreEqual("-1", engine.Process("1 -1 ÷"));
-            Assert.AreEqual("-1", engine.Process("-1 1 ÷"));
-        }
-
-        [TestMethod()]
-        public void BasicCalcuationFiveTest()
-        {
-            Assert.AreEqual("0.5", engine.Process("1 2 ÷"));
-            Assert.AreEqual("0.3333", engine.Process("1 3 ÷"));
-            Assert.AreEqual("0.25", engine.Process("1 4 ÷"));
-            Assert.AreEqual("0.1667", engine.Process("1 6 ÷"));
-            Assert.AreEqual("0.125", engine.Process("1 8 ÷"));
-        }
-
-        [TestMethod()]
-        public void ComplexCalcuationTest()
-        {
-            Assert.AreEqual("3", engine.Process("1 1 1 + + "));
-            Assert.AreEqual("-1", engine.Process("1 1 1 + - "));
-            Assert.AreEqual("1", engine.Process("1 1 1 - + "));
-            Assert.AreEqual("1", engine.Process("1 1 1 - - "));
-            Assert.AreEqual("6", engine.Process("2 2 2 X + "));
-            Assert.AreEqual("8", engine.Process("2 2 2 + X "));
-            Assert.AreEqual("0.5", engine.Process("2 2 2 + ÷"));
-            Assert.AreEqual("3", engine.Process("2 2 2 ÷ +"));
-        }
-
-        [TestMethod()]
-        public void DividedByZeroTest()
-        {
-            Assert.AreEqual("E", engine.Process("0 0 ÷ "));
-            Assert.AreEqual("E", engine.Process("1 0 ÷ "));
-            Assert.AreEqual("E", engine.Process("1 2 2 - ÷ "));
-        }
-
-        [TestMethod()]
-        public void InvalideFormatTest()
-        {
-            Assert.AreEqual("E", engine.Process("+"));
-            Assert.AreEqual("E", engine.Process("1+"));
-            Assert.AreEqual("E", engine.Process("+1"));
-            Assert.AreEqual("E", engine.Process("1 +"));
-            Assert.AreEqual("E", engine.Process("+ 1"));
-            Assert.AreEqual("E", engine.Process("1 1"));
-            Assert.AreEqual("E", engine.Process("+ 1 1"));
-            Assert.AreEqual("E", engine.Process("1 1 ++"));
-            Assert.AreEqual("E", engine.Process("1 1 + +"));
-            Assert.AreEqual("E", engine.Process("1 1 ++ +"));
-            Assert.AreEqual("E", engine.Process("1 1 + + +"));
-            Assert.AreEqual("E", engine.Process("1 1 1 + "));
-            Assert.AreEqual("E", engine.Process("1 1 1 + "));
         }
     }
 }
